0ebb0884be51abd608ffa3ef6dfadef6335fd244
Giuseppe De Marco <giuseppe.demarco@unical.it>
2019-05-20 15:38:26 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='filesig',
+      version='0.3',
+      description="Python command on top of poppler-utils and openssl used to verify file signatures",
+      long_description=readme(),
+      classifiers=['Development Status :: 5 - Production/Stable',
+                  'License :: OSI Approved :: BSD License',
+                  'Programming Language :: Python :: 2',
+                  'Programming Language :: Python :: 3'],
+      url='https://github.com/peppelinux/Remmina-password-exposer',
+      author='Giuseppe De Marco',
+      author_email='giuseppe.demarco@unical.it',
+      license='BSD',
+      scripts=['filesig.py'],
+      packages=['filesig.py'],
+      install_requires=[],
+     )
